5884814a010105bf935e53a1d7e4a20bb9a1cedd
Jeylon Guidi <jjguidi@student.fullsail.edu>
2024-02-08 17:51:36 -0800

    Added Scripts for moving platforms
    I made some changes to the original PlatformMover scripts, and then created a new separate PlatformMover script that used waypoints to travel along a path. A waypoint path script and a platform attachment script were also created to assist the platform movers.

--- StormBisonPrototype/Assets/Scripts/PlatformMover.cs
+++ StormBisonPrototype/Assets/Scripts/PlatformMover.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//[RequireComponent(typeof(PlatformAttachment))] //the platform with script will need a platform attachment script to work properly
 public class PlatformMover : MonoBehaviour
 {
     [SerializeField] Transform startPosition; //the platforms starting position (these should be emtpy game objects placed at the desired positions for the platform)
@@ -17,11 +18,12 @@ public class PlatformMover : MonoBehaviour
 
     }
 
-    void Update()
+    void FixedUpdate() //fixed update is needed for moving platforms since the fixed frame rates keeps the player from being unsynced with the platform
     {
-        if (movementEnabled) //TODO: Implement a stop when the platform reaches one of the positions
+        if (movementEnabled) 
         {
             journeyPercentage = Mathf.PingPong(Time.time * platformSpeed, 1f); //mathf ping pong can be used to oscillate between 0 and 1, which gives us a percentage to use for interpolation
+            journeyPercentage = Mathf.SmoothStep(0,1, journeyPercentage); //smooth out the movement when the platform approaches one of the two points 
             transform.position = Vector3.Lerp(startPosition.position, endPosition.position, journeyPercentage); //lerp interpolates between the two points based on the journey percentage 
         }
     }
