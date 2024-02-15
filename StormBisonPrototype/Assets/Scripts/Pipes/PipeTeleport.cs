6a72e2179944f080634c6636b467feb3b15ef5ae
Jeylon Guidi <jjguidi@student.fullsail.edu>
2024-02-14 19:02:30 -0800

    Added A Platforming Segment & Fixed Pipe Scripts
    fixed the pipe scripts to use a Coroutine to disable the exit pipe cover, instead of contantly checking for player height.

--- StormBisonPrototype/Assets/Scripts/Pipes/PipeTeleport.cs
+++ StormBisonPrototype/Assets/Scripts/Pipes/PipeTeleport.cs
@@ -6,15 +6,16 @@ public class PipeTeleport : MonoBehaviour
 {
     [SerializeField] Transform teleportPosition; //set the transform positon of the exit pipe
     [SerializeField] Vector3 LaunchForce; //set the launch force of the exit pipe
+    [SerializeField] ExitPipe exitPipeCover; //the exit pipe script for the corresponding exit pipes cover
 
     private void OnTriggerEnter(Collider other) //when the player enters the trigger volume
     {
         if (other.CompareTag("Player")) //check if the other object is the player
         {
+            if (exitPipeCover != null) { exitPipeCover.disableCover(); } //if the exit pipe is set then call its disable method before teleporting
             gameManager.instance.player.transform.position = teleportPosition.position; //set their position to that of the exit position
             Physics.SyncTransforms(); //tell the physics systems to sync transforms in order for the teleport to work
-            gameManager.instance.playerScript.PipeLaunch(LaunchForce); //call the pipe launch method on the player passing it the launch force
-            
+            gameManager.instance.playerScript.PipeLaunch(LaunchForce); //call the pipe launch method on the player passing it the launch force        
         }
 
     }
