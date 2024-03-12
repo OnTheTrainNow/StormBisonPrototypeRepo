eb16c87c25ecd13775fb93d811b8a7be33effe29
Jeylon Guidi <jjguidi@student.fullsail.edu>
2024-03-12 14:11:54 -0700

    Added most of the functionality for the dataManager script
    the data manager should now be able to handle coordination of any IPersistence object.
    It gathers them in a list, and has methods to call all of their save/load functions.
    The file handler reading and writing functionality still needs to be implemented for anything to function.

--- StormBisonPrototype/Assets/Scripts/DataPersistence/DataManager.cs
+++ StormBisonPrototype/Assets/Scripts/DataPersistence/DataManager.cs
@@ -1,18 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq; //this is used in getSaveDataList to find objects using the persistence interface (you can't search for Interfaces normally without this)
 
 public class DataManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] string fileName;
+
+    public static DataManager instance; //the data manager is a singleton that goes onto the scene
+
+    PlayerSaveData saveData; //this is the actual save data object used for all scripts 
+    List<IPersistence> hasSaveData; //this list will be used to find all objects in the scene that inherit from the persistence interface
+
+    SaveFileHandler saveFileHandler; //the save file handler class has the functionality for reading/writing the save data to a file
+
+    private void Awake()
     {
-        
+        instance = this; //there should be only one data manager in a scene
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Start()
     {
-        
+        saveFileHandler = new SaveFileHandler(); //create a fileHandler
+        getSaveDataList(); //get the list of IPersistence objects
+        loadPlayerData(); //load the players save data
+    }
+
+    public void getSaveDataList() //this method gets all the objects in the scene that inherit from IPersistence
+    {
+        hasSaveData = FindObjectsOfType<MonoBehaviour>().OfType<IPersistence>().ToList(); //find all the IPersistence objects in the save and save them to the list
+    }
+
+    public void createNewSaveFile() //this method creates a save data if one doesnt exist already
+    {
+        if (saveData == null) //if the save file doesnt exist
+        {
+            saveData = new PlayerSaveData(); //create one
+        }
+    }
+
+    public void savePlayerData() //this method calls the save methods of each IPersistence object
+    {
+        foreach (IPersistence pesistentDataObject in hasSaveData) //iterate over each IPersistence object
+        {
+            pesistentDataObject.SavePersistentData(ref saveData); //call their save method (it changes the save data by reference)
+        }
+
+        saveFileHandler.writeData(saveData); //tell the file handler to write the save data to a file
+    }
+
+    public void loadPlayerData() //this method calls the load methods for each of the IPersistence objects
+    {
+        saveData = saveFileHandler.readData(); //tell the file handler to read the save data from the file
+        createNewSaveFile(); //this method checks if saveData exists or not 
+
+        foreach (IPersistence pesistentDataObject in hasSaveData) //iterate over each IPersistence object
+        {
+            pesistentDataObject.LoadPersistentData(saveData); //call their load method passing in the save data as a parameter
+        }
     }
 }
