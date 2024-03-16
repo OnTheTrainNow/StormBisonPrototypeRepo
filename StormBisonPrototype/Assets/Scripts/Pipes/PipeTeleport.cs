99cc0f84f31a599bfaca0a127c891e130c794f7c
Jeylon Guidi <jjguidi@student.fullsail.edu>
2024-03-16 02:50:32 -0700

    Sewer Level Update & Two Way Pipes
    The level layout for the sewer level is basically done at this point.
    Pipes now have the ability to go two ways.

--- StormBisonPrototype/Assets/Scripts/Pipes/PipeTeleport.cs
+++ StormBisonPrototype/Assets/Scripts/Pipes/PipeTeleport.cs
@@ -8,6 +8,8 @@ public class PipeTeleport : MonoBehaviour
     [SerializeField] Vector3 LaunchForce; //set the launch force of the exit pipe
     [SerializeField] ExitPipe exitPipeCover; //the exit pipe script for the corresponding exit pipes cover
 
+    [SerializeField] bool isTwoWayPipe;
+
     [SerializeField] AudioSource enterSFX;
 
     private void OnTriggerEnter(Collider other) //when the player enters the trigger volume
@@ -18,7 +20,8 @@ public class PipeTeleport : MonoBehaviour
             {
                 enterSFX.Play();
             }
-            if (exitPipeCover != null) { exitPipeCover.disableCover(); } //if the exit pipe is set then call its disable method before teleporting
+            if (!isTwoWayPipe && exitPipeCover != null) { exitPipeCover.disableCover(); } //if the exit pipe is set then call its disable method before teleporting
+            if (isTwoWayPipe) { exitPipeCover.disableTrigger(); } //if its a tool way pipe then the trigger version of disable pipecover gets called
             gameManager.instance.player.transform.position = teleportPosition.position; //set their position to that of the exit position
             Physics.SyncTransforms(); //tell the physics systems to sync transforms in order for the teleport to work
             gameManager.instance.playerScript.Launch(LaunchForce); //call the pipe launch method on the player passing it the launch force        
