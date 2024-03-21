949acf4302f3cf1f1068b2e3053178a129821c7e
Jeylon Guidi <jjguidi@student.fullsail.edu>
2024-03-21 05:26:05 -0700

    Added Red Coin Manager, and Star Spawning Floor Button
    -The Red Coin Manager Spawns a Star Once All Red Coins in the Map Are Collected
    -The Star Button Spawns a Star When "Pressed"
    
    These will be used as part of the level objectives.

---
+++ StormBisonPrototype/Assets/Scripts/RedCoinManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedCoinManager : MonoBehaviour
+{
+    public static RedCoinManager instance;
+
+    [SerializeField] GameObject Star;
+    [SerializeField] int positionIndex;
+    [SerializeField] int StarID;
+    [SerializeField] GameObject SpawnPositon;
+
+    bool activated;
+    int redCoinCount;
+
+    MeshRenderer render;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        render = GetComponent<MeshRenderer>();
+        if (render != null) //turn the renderer off for the manager
+        {
+            render.enabled = false;
+        }
+    }
+
+    public void UpdateRedCoins(int amount)
+    {
+        redCoinCount += amount;
+
+        if (redCoinCount <= 0 && !activated)
+        {
+            activated = true;
+            GameObject spawned = Instantiate(Star, SpawnPositon.transform.position, SpawnPositon.transform.rotation);
+            spawned.GetComponent<Star>().positionIndex = positionIndex;
+            spawned.GetComponent<Star>().starArrayID = StarID;
+        }
+    }
+}
